4c58eee3892285d0664e97915033630e5149f67c
ceres r <ceres r>
2016-06-18 23:43:35 -0300

    :rocket: adiciona modelo do tpv.

---
+++ Tpv/Tpv.Model/Tpv.cs
@@ -0,0 +1,17 @@
+﻿namespace Tpv.Model
+{
+    public class Tpv
+    {
+        double Total { get; set; }
+        double LastYear { get; set; }
+        double ThisYear { get; set; }
+        double Last6Months { get; set; }
+        double LastMonth { get; set; }
+        double ThisMonth { get; set; }
+        double Last7Days { get; set; }
+        double Yesterday { get; set; }
+        double WithinTheMonth { get; set; }
+        double WithinTheDay { get; set; }
+        double Goal { get; set; }
+    }
+}
