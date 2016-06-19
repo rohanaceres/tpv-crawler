cdf1cc25d9074754e60557b5e29668dbacc53564
ceres r <ceres r>
2016-06-18 23:26:06 -0300

    :rocket: adiciona login
    Adiciona funcionalidade de login na dashboard
    da Stone.

---
+++ Tpv/Tpv.Crawler/TpvCrawler.cs
@@ -0,0 +1,45 @@
+﻿using NSoup;
+using NSoup.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Tpv.Crawler
+{
+    public class TpvCrawler
+    {
+        internal Uri TpvDashboardUri { get; set; }
+
+        public TpvCrawler()
+        {
+            this.TpvDashboardUri = new Uri(@"https://dashboards.stone.com.br:9002/viewer/login");
+        }
+
+        public bool Login (string userName, string password)
+        {
+            IResponse initialResponse = NSoupClient.Connect(this.TpvDashboardUri.AbsoluteUri)
+                .Method(Method.Get)
+                .Execute();
+            Document doc = initialResponse.Parse();
+            Element execution = doc.Select("input[id=antiforgery]").First;
+
+            string afval = execution.Attributes.GetValue("value");
+
+            IDictionary<string, string> cookies = initialResponse.Cookies();
+
+            Debug.WriteLine(initialResponse.StatusCode());
+
+            IResponse loginResponse = NSoupClient.Connect(this.TpvDashboardUri.AbsoluteUri)
+                .Data("user", userName)
+                .Data("password", password)
+                .Data("__RequestVerificationToken", afval)
+                .Cookies(cookies)
+                .Method(Method.Post)
+                .Execute();
+
+            Debug.WriteLine(loginResponse.StatusCode());
+
+            return true;
+        }
+    }
+}
